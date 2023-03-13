eb95e1e28e645a82f1be87201b66410bf6512482
Brian Faust <hello@basecode.sh>
2023-03-13 13:04:57 +0200

    chore: apply global pint configuration

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,14 +5,8 @@ declare(strict_types=1);
 namespace PreemStudio\MagicLinks;
 
 use PreemStudio\Jetpack\Package\AbstractServiceProvider;
-use PreemStudio\Jetpack\Package\Package;
 
 final class ServiceProvider extends AbstractServiceProvider
 {
-    public function configurePackage(Package $package): void
-    {
-        $package
-            ->name('laravel-magic-links')
-            ->hasRoute('web');
-    }
+    //
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,14 +5,16 @@ declare(strict_types=1);
 namespace Tests;
 
 use Laravel\Fortify\FortifyServiceProvider;
-use Orchestra\Testbench\TestCase as Orchestra;
+use PreemStudio\Jetpack\Tests\AbstractTestCase;
 use PreemStudio\MagicLinks\ServiceProvider;
+use Spatie\LaravelData\LaravelDataServiceProvider;
 
-abstract class TestCase extends Orchestra
+abstract class TestCase extends AbstractTestCase
 {
     protected function getPackageProviders($app): array
     {
         return [
+            LaravelDataServiceProvider::class,
             FortifyServiceProvider::class,
             ServiceProvider::class,
         ];
